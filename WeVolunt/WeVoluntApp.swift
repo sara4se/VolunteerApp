b90bc5bf87489813b0ad916ea1b3af6a79316927
Roba gh <rfalghanmi@gmail.com>
2022-12-06 13:02:33 +0300

    Initial Commit

---
+++ WeVolunt/WeVoluntApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeVoluntApp.swift
+//  WeVolunt
+//
+//  Created by roba on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeVoluntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
