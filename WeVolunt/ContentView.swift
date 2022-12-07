625c6fd34882ac673775fc3474ea63c9c6c12c23
Roba gh <rfalghanmi@gmail.com>
2022-12-07 10:48:56 +0300

    first commit

--- WeVolunt/ContentView.swift
+++ WeVolunt/ContentView.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var splashScreen  = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+        //this will view the splash screen for 2 sec then switch to main page
+        ZStack{
+            Group{
+                if splashScreen {
+                    SplashScreen()
+                }
+                else{
+                    InterestPage()
+                }
+            }
+            .onAppear {
+                DispatchQueue
+                    .main
+                    .asyncAfter(deadline:
+                            .now() + 2) {
+                                self.splashScreen = false
+                            }
+            }
+        }    }
 }
 
 struct ContentView_Previews: PreviewProvider {
