3dff2e0c25cace7912ef876e25889a7dff89a1f3
sara <sara.ahumidi4@gmail.com>
2022-12-16 13:45:56 +0300

    last changes

--- WeVolunt/InterestPage.swift
+++ WeVolunt/InterestPage.swift
@@ -7,39 +7,135 @@
 
 import SwiftUI
 
+struct Title : Identifiable {
+    var titleStr = ""
+    var fontsize = 0
+    var id = UUID()
+    
+    
+}
+
 struct InterestPage: View {
+    
+    
+    @StateObject var userSettings : UserSettings = UserSettings(enterdInterstTogle: true, username: "", isPrivate: true, arrayOfSelected: [])
+    @State var itemsTitles: [Title] = [Title(titleStr:"Environmental" , fontsize : 60) ,Title(titleStr:"Sports" , fontsize : 40),Title(titleStr:"Social" , fontsize : 40), Title(titleStr:"Religious" , fontsize : 60),Title(titleStr:"Technical" , fontsize : 50),Title(titleStr:"Education" , fontsize : 50),Title(titleStr:"Entertainment" , fontsize : 60) ,Title(titleStr:"Health" , fontsize : 40)]
+    
+    //  @State var selections: [String] = []
+    let layout = [
+        GridItem(.adaptive(minimum: 130, maximum: 120))
+    ]
+    
+    @State var selected = 0    // 1
+    
     
     var body: some View {
         NavigationView(){
-            ZStack{
-                // *** background should be here ***
+            VStack{
                 VStack{
-                    // *** START PAGE CONTENT ***
-                    Text("This is interest page")
+                    Text("Pick your interset").foregroundColor(Color.lightBlue).font(Font.custom("SF-Compact", size: CGFloat(30))).padding(.bottom,30).padding(.trailing,94).padding(.leading,39)
+                }
+                ScrollView(.horizontal,showsIndicators: true){
+                    HStack{
+                        ForEach(userSettings.arrayOfSelected, id: \.self) { selectd in
+                            Text(selectd).padding(6)
+                                .background(Color.skipColor).foregroundColor( Color.white)
+                                .cornerRadius(5).animation(Animation.easeIn(duration: 2.0))
+                            
+                        }
+                    }
+                }.frame(width: CGFloat(340) ,height:CGFloat(50) ,alignment: .leading)
+                
+                VStack(spacing: 1){
+                    ScrollView(.vertical){
+                        LazyVGrid(columns: layout, spacing: 10) {
+                            ForEach(self.itemsTitles) { item in
+                                MultipleSelectionRow(title: item.titleStr, isSelected: userSettings.arrayOfSelected.contains(item.titleStr) , font: UIFont.systemFont(ofSize:CGFloat(item.fontsize) , weight: .light) ) {
+                                    if userSettings.arrayOfSelected.contains(item.titleStr) {
+                                        userSettings.arrayOfSelected.removeAll(where: { $0 == item.titleStr })
+                                    }
+                                    else {
+                                        userSettings.arrayOfSelected.append(item.titleStr)
+                                        self.itemsTitles.removeAll(where: {$0.id == item.id})
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }.frame(width: CGFloat(300),height: CGFloat(350),alignment: .leading).padding(.bottom,1).padding(.top,1)
                     
-                    //next button
-                    NavigationLink(destination: MainPage(categoriesList: categoriesList, volunteersList: volunteerList), label:{
+                }.frame(width: CGFloat(300),height: CGFloat(350),alignment: .leading)
+                
+                
+                //next button
+                
+                VStack{
+                    NavigationLink(destination:   MainPage( categoriesList: categoriesList, volunteersList: volunteerList), label:{
                         Text("Next")
-                            .frame(width:307 , height:44 )
+                            .frame(width:281 , height:41 )
                             .foregroundColor(.white)
-                            .background(Color(.blue))
+                            .background(Color.darkBlue)
                             .cornerRadius(8)
                             .fontWeight(.semibold)
                     })
+                }
+                
+                .toolbar{
                     
-                    .toolbar{
+                    NavigationLink(destination:  MainPage( categoriesList: categoriesList, volunteersList: volunteerList), label:{
+                        Text("Skip").foregroundColor(Color.skipColor).font(Font.custom("SF-Compact", size: CGFloat(20)))
                         
-                        NavigationLink(destination: MainPage(categoriesList: categoriesList, volunteersList: volunteerList), label:{
-                            Text("Skip")
-                            
-                        })
-                    }
+                    })
                 }
             }
+        }.background(Color.backgroundColor)
+    }
+    
+    
+    
+    struct MultipleSelectionRow: View {
+        var title: String
+        var isSelected: Bool
+        
+        var font = UIFont.preferredFont(forTextStyle: .body)  // << default !!
+        
+        var action: () -> Void
+        
+        @State private var half = false
+        @State private var dim = false
+        
+        var body: some View {
+            let size = font.lineHeight * 2.0
+            
+            Button(action: self.action,
+                   label: {
+                HStack {
+                    Text(self.title)
+                    if self.isSelected {
+                    }
+                }
+            }).frame(width: size, height: 49)
+                .foregroundColor(self.isSelected ? Color.white : Color.lightBlue)
+                .background(self.isSelected ? Color.skipColor : Color.backgroundColor)
+                .overlay( /// apply a rounded border
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(.blue, lineWidth: 1)
+                )
+                .scaleEffect(half ? 1.0  : 0.5)
+                .opacity(dim ? 1.0 : 0.2 )
+                .animation(Animation.easeInOut(duration: 1.0))
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3, execute: {
+                        self.dim.toggle()
+                        self.half.toggle()
+                    }
+                                                  
+                                                  
+                    )}
+            
         }
     }
 }
-
 struct InterestPage_Previews: PreviewProvider {
     static var previews: some View {
         InterestPage()
--- WeVolunt/ProfilePage.swift
+++ WeVolunt/ProfilePage.swift
@@ -10,27 +10,90 @@ import SwiftUI
 struct ProfilePage: View {
     @State var showingVolunteerSheet = false
     var body: some View {
-        ZStack{
-            // *** background should be here ***
-            VStack{
-                // *** START PAGE CONTENT ***
-                Text("this is profile page")
-                    .padding()
-                Text("click here to open selected volunteer")
-                    .onTapGesture(perform: {
-                        showingVolunteerSheet.toggle()
-                    })
-                    .fullScreenCover(isPresented: $showingVolunteerSheet){
-                        EnrolledVolunteer(showingVolunteerSheet:$showingVolunteerSheet)
+
+            ZStack{
+                VStack {
+                    ZStack(alignment: .top){
+                        MyCustomShape2().fill(.linearGradient(colors: [Color.darkBlue, Color.lightBlue,Color.babyBlue], startPoint: .leading, endPoint: .trailing)).frame(width: CGFloat(400),height: CGFloat(400)).ignoresSafeArea()
+                        MyCustomShape3().padding(.bottom,230).padding(.leading,25).foregroundColor(.backgroundColor).frame(width: CGFloat(400),height: CGFloat(400)).ignoresSafeArea()
                     }
+                }.padding(.bottom,370)
+                VStack (alignment: .center){
+                    HStack {
+                        Text("Hello Ahmad").font(Font.custom("SF-Compact", size: CGFloat(25))).foregroundColor(.white)
+                    }.frame(width: .infinity,height: 2,alignment: .leading).padding(.trailing,210)
+                   
+                    //.padding(.trailing,150).padding(.bottom,100)
+                    ZStack{
+                        Circle()
+                            .strokeBorder(.linearGradient(colors: [Color.DarkOrange,Color.LightOrange,Color.LightOrange], startPoint: .bottomLeading, endPoint: .topTrailing), lineWidth: 30)
+                            .background(Circle().fill(Color.backgroundColor))
+                            .frame(width: 202, height: 198)
+                            .overlay {
+                                HStack{
+                                    Text("17").font(Font.custom("SF-Compact", size: CGFloat(63))).foregroundColor(Color.darkBlue).frame(alignment : .center)
+                                    Text("hrs").font(Font.custom("SF-Compact", size: CGFloat(16))).foregroundColor(Color.darkBlue).frame(width: 23,height: 50,alignment : .bottomTrailing)
+                                }
+                            }
+                    }.frame(width: .infinity,height: 2,alignment: .leading).padding(.top,100)
+                    
+                    Spacer()
+                    
+                    HStack (alignment: .top){
+                        Text("Total hours of volunteer").font(Font.custom("SF-Compact", size: CGFloat(25))).foregroundColor(Color.skipColor)
+                    }.frame(width: .infinity,height: 50,alignment: .center).padding(.top,20)
+                    
+                    Divider().frame(width: 300,height: 1)
+                    
+                    HStack (alignment:.top){
+                        Text("Enrolld in").font(Font.custom("SF-Compact", size: CGFloat(25))).fontWeight(.bold).foregroundColor(Color.darkBlue).multilineTextAlignment(.leading)
+                    }.frame(width: .infinity,height: 50,alignment: .leading).padding(.top,20).padding(.trailing,240)
+                    
+                    ScrollView(.horizontal){
+                        HStack(spacing:30){
+                        
+                            
+                        }
+                        .padding()
+                    }.frame(width: 360,height:233,alignment: .bottom)
+                    Spacer()
+                }.padding(.top,140)
+                
+            }.background(Color.backgroundColor).toolbar{
+                
+
+                ToolbarItem(placement: .principal) { // <3>
+                    VStack {
+                        Text("Profile").font(.subheadline).foregroundColor(.white)
+                     
+                    }
+
+                
             }
-            
-        }
+        }.background(Color.backgroundColor).ignoresSafeArea()
     }
 }
 
 struct ProfilePage_Previews: PreviewProvider {
     static var previews: some View {
         ProfilePage()
+    }
+}
+struct MyCustomShape2: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.addEllipse(in: CGRect(x: -0.16923*width, y: -0.24124*height, width: 1.35641*width, height: 1.24124*height))
+        return path
+    }
+}
+struct MyCustomShape3: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.addEllipse(in: CGRect(x: -0.10256*width, y: 1.54974*height, width: 1.14359*width, height: height))
+        return path
     }
 }
